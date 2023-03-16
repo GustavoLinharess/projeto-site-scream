664d7dbd9ba299aa1c7dba56ce0235a5ae2bd76c
GustavoLinhares <99147725+GustavoLinharess@users.noreply.github.com>
2023-03-16 20:44:42 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,47 @@
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+
+        desativarBotaoSelecionado();
+
+        selecionarBotaoCarrossel(botao);
+
+        esconderImagemAtiva();
+
+        mostrarImagemDeFundo(indice);
+    })
+})
+
+function mostrarImagemDeFundo(indice) {
+    imagens[indice].classList.add('ativa');
+}
+
+function selecionarBotaoCarrossel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+}
+
+
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+    if(idx > imagens.length - 1){
+        idx = 0;
+    }
+    imagens.style.transform ='translateX(${-dx * 1280})px';
+}
+
+setInterval(carrossel, 1800);
